39c02f75abbf5d52d28d439c01b301320b118447
Sebastian (Felvesthe) <felvesthe@outlook.com>
2019-08-08 22:24:28 +0200

    msm8998-common: Introduce DeviceParts * Based on https://github.com/Havoc-Devices/android_device_oneplus_settings
    Signed-off-by: Sebastian (Felvesthe) <felvesthe@outlook.com>

--- common.mk
+++ common.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# OnePlus Settings
+PRODUCT_PACKAGES += \
+    DeviceParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
